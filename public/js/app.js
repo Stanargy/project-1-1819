df8f8d008046c452c08ef86bae483a274f107055
Stan Ooms <35112551+Stanargy@users.noreply.github.com>
2019-03-06 15:35:49 +0100

    work in progress

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,18 @@
 "use strict";
+
+function callAPI(){
+    var endpoint = 'https://zoeken.oba.nl/api/v1/';
+    var publicKey = '&authorization=' + '1e19898c87464e239192c8bfe422f280';
+    var secretKey = '4289fec4e962a33118340c888699438d';
+    var query = 'harry';
+    var url = endpoint + 'search/?q='+query+publicKey;
+    console.log(url)
+ fetch(url, {mode: 'no-cors'})
+ .then(res => res.json())
+ .then(data => console.log(data))
+    .catch((err) =>{
+        console.log(err);
+ })
+}
+
+callAPI();
